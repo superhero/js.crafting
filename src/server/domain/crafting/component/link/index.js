13362a96f6ffb1dda6234b6cc35335f2be63efe0
Erik Landvall <erik@landvall.se>
2023-07-23 19:11:58 +0200

    v 52

---
+++ src/server/domain/crafting/component/link/index.js
@@ -0,0 +1,9 @@
+const Component = require('..')
+
+class CraftingComponentText extends Component
+{
+  schema    = __dirname + '/schema'
+  template  = __dirname + '/template'
+}
+
+module.exports = CraftingComponentText
---
+++ src/server/domain/crafting/component/link/schema.js
@@ -0,0 +1,17 @@
+/**
+ * @memberof Server.Domain
+ * @typedef {Object} CraftingText
+ */
+const schema =
+{
+  url:
+  {
+    'type' : 'string'
+  },
+  label:
+  {
+    'type' : 'string'
+  }
+}
+
+module.exports = schema
